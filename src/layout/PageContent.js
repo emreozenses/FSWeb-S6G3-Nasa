4430f20416f17de21b5154eaf1abf1a83124d08f
Emre OZENSES <emreozenses@gmail.com>
2023-11-07 22:34:47 +0300

    latest update -task completed

--- src/layout/PageContent.js
+++ src/layout/PageContent.js
@@ -4,25 +4,25 @@ import {
   CarouselItem,
   CarouselControl,
   CarouselIndicators,
-  CarouselCaption,
 } from "reactstrap";
 
 const PageContent = (props) => {
   const { pictureData, date, changeHandler } = props;
-  const { items } = pictureData.props;
 
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
   const next = () => {
     if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    const nextIndex =
+      activeIndex === pictureData.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   };
 
   const previous = () => {
     if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    const nextIndex =
+      activeIndex === 0 ? pictureData.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   };
 
@@ -35,13 +35,18 @@ const PageContent = (props) => {
     <div className="page-content">
       <input type="date" name="tarih" value={date} onChange={changeHandler} />
       <div className="page-content-main">
-        <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+        <Carousel
+          activeIndex={activeIndex}
+          next={next}
+          previous={previous}
+          
+        >
           <CarouselIndicators
-            items={items}
+            items={pictureData}
             activeIndex={activeIndex}
             onClickHandler={goToIndex}
           />
-          {items && items.map((item) => {
+          {pictureData.map((item) => {
             return (
               <CarouselItem
                 onExiting={() => setAnimating(true)}
@@ -49,13 +54,9 @@ const PageContent = (props) => {
                 key={item.url}
               >
                 <h4>{item.title}</h4>
-                <img src={item.hdurl} alt="apod" />
+                <img id="apod" src={item.hdurl} alt="apod" />
                 <p id="exp">{item.explanation}</p>
                 <span>{item.date}</span>
-                <CarouselCaption
-                  captionText={item.title}
-                  captionHeader={item.title}
-                />
               </CarouselItem>
             );
           })}
